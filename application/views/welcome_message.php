57a0da08ffe53df77e987e23214b7a781aebfe48
makkaba <drifterz303@naver.com>
2016-03-30 17:34:57 +0900

    copy index from opensource.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -72,6 +72,72 @@ $loginUrl = $instagram->getLoginUrl();
         </footer>
     </div>
 </div>
+<div class="container">
+    <header class="clearfix">
+        <img src="assets/instagram.png" alt="Instagram logo">
+
+        <h1>Instagram photos <span>taken by <?php echo $data->user->username ?></span></h1>
+    </header>
+    <div class="main">
+        <ul class="grid">
+            <?php
+            // display all user likes
+            foreach ($result->data as $media) {
+                $content = '<li>';
+                // output media
+                if ($media->type === 'video') {
+                    // video
+                    $poster = $media->images->low_resolution->url;
+                    $source = $media->videos->standard_resolution->url;
+                    $content .= "<video class=\"media video-js vjs-default-skin\" width=\"250\" height=\"250\" poster=\"{$poster}\"
+                           data-setup='{\"controls\":true, \"preload\": \"auto\"}'>
+                             <source src=\"{$source}\" type=\"video/mp4\" />
+                           </video>";
+                } else {
+                    // image
+                    $image = $media->images->low_resolution->url;
+                    $content .= "<img class=\"media\" src=\"{$image}\"/>";
+                }
+                // create meta section
+                $avatar = $media->user->profile_picture;
+                $username = $media->user->username;
+                $comment = $media->caption->text;
+                $content .= "<div class=\"content\">
+                           <div class=\"avatar\" style=\"background-image: url({$avatar})\"></div>
+                           <p>{$username}</p>
+                           <div class=\"comment\">{$comment}</div>
+                         </div>";
+                // output media
+                echo $content . '</li>';
+            }
+            ?>
+        </ul>
+        <!-- GitHub project -->
+        <footer>
+            <p>created by <a href="https://github.com/cosenary/Instagram-PHP-API">cosenary's Instagram class</a>,
+                available on GitHub</p>
+            <iframe width="95px" scrolling="0" height="20px" frameborder="0" allowtransparency="true"
+                    src="http://ghbtns.com/github-btn.html?user=cosenary&repo=Instagram-PHP-API&type=fork&count=true"></iframe>
+        </footer>
+    </div>
+</div>
+<!-- javascript -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script>
+    $(document).ready(function () {
+        // rollover effect
+        $('li').hover(
+            function () {
+                var $media = $(this).find('.media');
+                var height = $media.height();
+                $media.stop().animate({marginTop: -(height - 82)}, 1000);
+            }, function () {
+                var $media = $(this).find('.media');
+                $media.stop().animate({marginTop: '0px'}, 1000);
+            }
+        );
+    });
+</script>
 
 
 
@@ -86,14 +152,13 @@ $loginUrl = $instagram->getLoginUrl();
 
 if(isset($_GET['code']) === true){
 	$code = $_GET['code'];
-	//var_dump($code);
-	
-	session_start();
-	$_SESSSION['user_token'] = $code;
-	//$data = $instagram->getOAuthToken($code);
-    
-    //echo 'Your username is: ' . $data->user->username;
-	
+	// receive OAuth token object
+    $data = $instagram->getOAuthToken($code);
+    $username = $data->user->username;
+    // store user access token
+    $instagram->setAccessToken($data);
+    // now you have access to all authenticated user methods
+    $result = $instagram->getUserMedia();
 }
 	
 ?>
