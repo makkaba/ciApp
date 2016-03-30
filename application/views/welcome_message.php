ec389976014157a9c5ce630a98f0546c20a6ed8d
makkaba <drifterz303@naver.com>
2016-03-30 18:49:23 +0900

    made curl request

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2,7 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 require_once ('Instagram.php');
-
+const API_OAUTH_TOKEN_URL = 'https://api.instagram.com/oauth/access_token';
+const MY_API_KEY = 'db4f35cd0f1042629eb500ca10af6810';
+const MY_SECRET = '2fc0e4f024464cdb9555dea61e422a70';
+const MY_RETURN = 'http://www.lionlove.me';
 /*
 TODO:vardump로 다 뿌려보고
 TODO:call 함수로 curl통신을 해서 데이터 가져오기.
@@ -15,6 +18,8 @@ $instagram = new Instagram(array(
 	'apiSecret'=>'2fc0e4f024464cdb9555dea61e422a70',
 	'apiCallback'=>'http://www.lionlove.me'
 ));
+
+
 // create login URL
 $loginUrl = $instagram->getLoginUrl();
 
@@ -22,13 +27,32 @@ $loginUrl = $instagram->getLoginUrl();
 if (isset($_GET['code'])) {
 	$code = $_GET['code'];
     // receive OAuth token object
-    $data = $instagram->getOAuthToken($code);
-    $username = $data->user->username;
-    // store user access token
-    $instagram->setAccessToken($data);
-    // now you have access to all authenticated user methods
-    $result = $instagram->getUserMedia();
-    var_dump($result);
+	$apiData = array(
+            'grant_type' => 'authorization_code',
+            'client_id' => MY_API_KEY,
+            'client_secret' => MY_SECRET,
+            'redirect_uri' => MY_RETURN,
+            'code' => $code
+	);
+
+
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $apiHost);
+        curl_setopt($ch, CURLOPT_POST, count($apiData));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($apiData));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 90);
+        $jsonData = curl_exec($ch);
+        if (!$jsonData) {
+            throw new InstagramException('Error: _makeOAuthCall() - cURL error: ' . curl_error($ch));
+        }
+        curl_close($ch);
+
+
+    var_dump($jsonData);
 } else {
     // check whether an error occurred
     if (isset($_GET['error'])) {
